1f97c50904e40e68b5d24a27b31df011aab33870
황상진 <sangjin5579@naver.com>
2020-02-22 23:58:52 +0900

    update

---
+++ src/components/Calendar.vue
@@ -0,0 +1,29 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            calendar: [{
+                id: '', //연월일 
+                schedule: [{
+                    Contents: '',
+                    startDate: '',
+                    endDate: '',
+                    startTime: '',
+                    endTime: '',
+                    Color: ''
+                }],
+                date: ''
+            }],
+                 
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
